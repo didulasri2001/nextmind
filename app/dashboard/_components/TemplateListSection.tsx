2fb479c09a907018eea8e8c303044ad9de9d370b
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-28 21:50:55 +0530

    feat: add styles to templates

--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -1,22 +1,43 @@
 import Templates from "@/app/(data)/Templates";
 import React from "react";
 
+export interface TemplateListSection {
+  name: string;
+  icon: string;
+  desc: string;
+  category: string;
+  slug: string;
+  aiPrompt: string;
+  form?: FORM[];
+}
+export interface FORM {
+  label: string;
+  field: string;
+  name: string;
+  required?: boolean;
+}
+
 function TemplateListSection() {
   return (
-    <div>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-5 ml-2">
       {Templates.map((template, index) => (
         <div
           key={index}
-          className="p-3 bg-white rounded-lg shadow-sm mb-3 flex items-center gap-3"
+          className="p-3 bg-white rounded-lg shadow-md mb-3 flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all"
         >
-          <img
-            src={template.icon}
-            alt={template.name}
-            className="w-20 h-20 object-cover rounded-lg"
-          />
-          <div>
+          <div className="flex items-center gap-5">
+            <img
+              src={template.icon}
+              alt={template.name}
+              className="w-10 h-10 object-cover rounded-lg"
+            />
             <h2 className="text-lg font-bold">{template.name}</h2>
-            <p className="text-sm text-gray-500">{template.desc}</p>
+          </div>
+
+          <div>
+            <p className="text-sm p-2 text-gray-500  line-clamp-3">
+              {template.desc}
+            </p>
           </div>
         </div>
       ))}
