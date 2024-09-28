3d5e22b1c7ba5a7369715e5a121be912c33a608a
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-28 20:28:22 +0530

    feat: add templates to dashboard

--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react";
 import React from "react";
 
 function SearchSection() {
@@ -5,6 +6,16 @@ function SearchSection() {
     <div className="p-10 bg-gradient-to-br from-[#f8e074] via-[#c0cb95] to-[#51a2d9] flex flex-col justify-center items-center text-white">
       <h2 className="text-3xl font-bold">Browse All Templates</h2>
       <p>What would you like to create today ?</p>
+      <div className="w-full flex justify-center ">
+        <div className="flex gap-4 items-center p-2  border rounded-md mt-5 bg-white w-[50%]">
+          <Search size={20} className="text-black" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="outline-none bg-transparent text-black"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -1,11 +1,26 @@
+import Templates from "@/app/(data)/Templates";
 import React from "react";
 
 function TemplateListSection() {
   return (
     <div>
-      <h2>temp</h2>
+      {Templates.map((template, index) => (
+        <div
+          key={index}
+          className="p-3 bg-white rounded-lg shadow-sm mb-3 flex items-center gap-3"
+        >
+          <img
+            src={template.icon}
+            alt={template.name}
+            className="w-20 h-20 object-cover rounded-lg"
+          />
+          <div>
+            <h2 className="text-lg font-bold">{template.name}</h2>
+            <p className="text-sm text-gray-500">{template.desc}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
-
 export default TemplateListSection;
