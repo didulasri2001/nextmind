fa9b60a3213f53af539c8cd1b232568a8ac44f4e
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-29 10:57:18 +0530

    feat: create form section

--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -1,4 +1,5 @@
 import Templates from "@/app/(data)/Templates";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 export interface TemplateListSection {
@@ -32,25 +33,27 @@ function TemplateListSection({ userSearchInput }: any) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-5 ml-2">
       {templateList.map((template, index) => (
-        <div
-          key={index}
-          className="p-3 bg-white rounded-lg shadow-md mb-3 flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all"
-        >
-          <div className="flex items-center gap-5">
-            <img
-              src={template.icon}
-              alt={template.name}
-              className="w-10 h-10 object-cover rounded-lg"
-            />
-            <h2 className="text-lg font-bold">{template.name}</h2>
-          </div>
+        <Link href={"/dashboard/content/" + template?.slug}>
+          <div
+            key={index}
+            className="p-3 bg-white rounded-lg shadow-md mb-3 flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all"
+          >
+            <div className="flex items-center gap-5">
+              <img
+                src={template.icon}
+                alt={template.name}
+                className="w-10 h-10 object-cover rounded-lg"
+              />
+              <h2 className="text-lg font-bold">{template.name}</h2>
+            </div>
 
-          <div>
-            <p className="text-sm p-2 text-gray-500  line-clamp-3">
-              {template.desc}
-            </p>
+            <div>
+              <p className="text-sm p-2 text-gray-500  line-clamp-3">
+                {template.desc}
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
---
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import FormSection from "../_components/FormSection";
+import OutputSection from "../_components/OutputSection";
+interface PROPS {
+  params: {
+    "template-slug": string;
+  };
+}
+
+function CreateNewContent(props: PROPS) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-5">
+      <FormSection />
+      <OutputSection />
+    </div>
+  );
+}
+
+export default CreateNewContent;
